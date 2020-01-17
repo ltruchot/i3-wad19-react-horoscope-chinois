35819fbe818d142272a04118ca7d76fa807ddd6a
Loïc TRUCHOT <loic.truchot.pro@gmail.com>
2020-01-17 12:22:07 +0100

    feat(usestate): add states for modal and selected sign

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react'; 
 import './App.scss';
 import Card from './Card';
 import FormControl from './FormControl';
 
 function App() {
+  const [mySign, setMySign] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  
   const fullSigns = [
     { name: 'singe', desc: 'est rigolo', flowers: ['chrysanthème'] },
     { name: 'coq', desc: 'se lève tôt', flowers: ['flower of fragrant', 'plantain', 'lily', 'nerve plant', 'snapdragon'] },
@@ -22,16 +25,42 @@ function App() {
   const checkYear = () => {
     const year = document.querySelector('input').value;
     const index = year % 12;
-    alert(`vous etes ${fullSigns[index].name}, un signe qui ${fullSigns[index].desc}`);
+    // alert(`vous etes ${fullSigns[index].name}, un signe qui ${fullSigns[index].desc}`);
+    setMySign(fullSigns[index].name);
+    setModalVisible(true);
   };
+
+  const closeModal = () => setModalVisible(false);
 
   return (
     <div className="container">
+
       <h1>Astrologie chinoise</h1>
       <FormControl text="Quel est votre année de naissance ?" type="number" />
       <button onClick={checkYear} type="button" className="btn btn-success">Savoir</button>
+      <div>Mon signe chinois: {mySign}</div>
       <div className="card-columns">
-        { fullSigns.map((item) => <Card sign={item} />) }
+        { fullSigns.map((item) => <Card callback={() => setModalVisible(true)} chosen={ mySign === item.name } sign={item} />) }
+      </div>
+
+      <div className="modal" style={{display: modalVisible ? "block" : "none", backgroundColor: "rgba(0,0,0,0.5)"}}>
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+              <button onClick={closeModal} type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <p>Modal body text goes here.</p>
+            </div>
+            <div className="modal-footer">
+              <button onClick={closeModal} type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Card = ({ sign }) => {
+const Card = ({ chosen, sign, callback }) => {
   const img = `images/${sign.name}.jpg`;
 
   return (
-    <div className="card">
+    <div
+      onClick={callback}
+      className="card"
+      style={{ border: chosen ? '5px solid red' : '0' }}
+    >
       <img src={img} className="card-img-top" alt="toto" />
       <div className="card-body">
         <h5 className="card-title">{sign.name}</h5>
